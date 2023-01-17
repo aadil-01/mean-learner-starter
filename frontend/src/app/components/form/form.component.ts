dde9d5290222a056f197161f24e9f8a34432cd31
Aadil Shaik <adil6888688@gmail.com>
2023-01-17 17:03:33 +0530

    UI Minor Fix

--- frontend/src/app/components/form/form.component.ts
+++ frontend/src/app/components/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { LearnerserviceService } from 'src/services/learnerservice.service';
 
 @Component({
@@ -10,12 +10,15 @@ export class FormComponent {
 
   task:string = ''
 
+  @Output() stateChanged = new EventEmitter<any>();
+
   constructor(private service:LearnerserviceService){}
 
   onSubmit(){
     if(this.task!==''){
       this.service.insertData({task: this.task, status: false}).subscribe(data=>{
         console.log('Task Added')
+        this.stateChanged.emit({changed:true})
         this.task = ''
       })
     }
