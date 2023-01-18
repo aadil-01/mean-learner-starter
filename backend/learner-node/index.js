4d16e4f3a8a6b85bc2ef466960122fd18642d137
Aadil Shaik <adil6888688@gmail.com>
2023-01-18 15:04:29 +0530

    swagger display issue in prod

--- backend/learner-node/index.js
+++ backend/learner-node/index.js
@@ -1,3 +1,4 @@
+import path from 'path';
 const express = require("express");
 const cors = require("cors");
 const { initDB } = require("./db/index");
@@ -7,6 +8,9 @@ const swaggerFile = require('./swagger/swagger_output.json');
 const { version } = require("./package.json");
 
 const app = express();
+
+const ROOT_FOLDER = path.join(__dirname, '..');
+const SRC_FOLDER = path.join(ROOT_FOLDER, 'src');
 
 // To enable cors
 app.use(cors({ origin: '*', methods: "GET,POST,PATCH,DELETE" }));
@@ -26,8 +30,12 @@ app.get('/', (req, res) => {
 //to use routes
 app.use(routes);
 
+
+const options = { customCssUrl: '/public/swagger-ui.css', customSiteTitle: "The Words That I Know API - Swagger" };
+app.use('/public', express.static(path.join(SRC_FOLDER, 'public')));
+
 //to use swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile,options))
 
 const PORT = 3000
 
